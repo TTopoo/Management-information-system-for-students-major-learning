162bbdc866dfd4bfbb9d5199ce8e358c10388969
jackensxzk <48912121+jackensxzk@users.noreply.github.com>
2020-04-13 10:53:37 +0800

    添加模型 更改表结构

--- login/models.py
+++ login/models.py
@@ -5,8 +5,9 @@ from django.db import models
 class User(models.Model):
     stuid = models.CharField(max_length=128, unique=True, verbose_name='学生ID')
     password = models.CharField(max_length=256, verbose_name='密码')
-    
+
     c_time = models.DateTimeField(auto_now_add=True)
+
     # salt = models.CharField(max_length=10, verbose_name='盐')
 
     def __str__(self):
@@ -44,6 +45,11 @@ class StudentInformationModel(models.Model):
         verbose_name_plural = '用户信息'
 
 
+# 教师信息模型
+
+# 管理员信息模型
+
+
 # 学生奖惩模型
 class StudentAwardsRecodeModel(models.Model):
     stu_id = models.ForeignKey('StudentInformationModel', on_delete=models.CASCADE)
@@ -57,16 +63,74 @@ class StudentAwardsRecodeModel(models.Model):
         verbose_name_plural = '奖惩信息'
 
 
+# 学院模型
+class CollegeModel(models.Model):
+    college_id = models.AutoField(primary_key=True, verbose_name='学院ID')
+    college_name = models.CharField(max_length=64, verbose_name='学院名称')
+
+    def __str__(self):
+        return self.college_name
+
+    class Meta:
+        ordering = ['college_name']
+        verbose_name = '学院'
+        verbose_name_plural = '学院'
+
+
+# 专业模型
+class MajorModel(models.Model):
+    major_id = models.AutoField(primary_key=True, verbose_name='专业ID')
+    major_name = models.CharField(max_length=64, verbose_name='专业名称')
+    college_id = models.ForeignKey('CollegeModel', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.major_name
+
+    class Meta:
+        ordering = ['major_name']
+        verbose_name = '专业'
+        verbose_name_plural = '专业'
+
+
 # 课程模型
 class CourseModel(models.Model):
     course_id = models.AutoField(primary_key=True, verbose_name='课程ID')
     course_name = models.CharField(max_length=64, verbose_name='课程名称')
-    create_date = models.DateTimeField(auto_now_add=True)
+    major_id = models.ForeignKey('MajorModel', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.course_name
 
     class Meta:
-        ordering = ['create_date']
+        ordering = ['course_name']
         verbose_name = '课程'
         verbose_name_plural = '课程'
+
+
+# 班级模型
+class ClassModel(models.Model):
+    class_id = models.AutoField(primary_key=True, verbose_name='班级ID')
+    class_name = models.CharField(max_length=64, verbose_name='班级名称')
+    major_id = models.ForeignKey('MajorModel', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.class_name
+
+    class Meta:
+        ordering = ['class_name']
+        verbose_name = '班级'
+        verbose_name_plural = '班级'
+
+
+# 课程-学生 连接表
+class CourseStudentModel(models.Model):
+    course_id = models.ForeignKey('CourseModel', on_delete=models.CASCADE)
+    student_id = models.ForeignKey('StudentInformationModel', on_delete=models.CASCADE)
+
+
+# 课程-教师 连接表
+
+# 班级-学生 连接表
+class ClassStudentModel(models.Model):
+    class_id = models.ForeignKey('ClassModel', on_delete=models.CASCADE)
+    student_id = models.ForeignKey('StudentInformationModel', on_delete=models.CASCADE)
